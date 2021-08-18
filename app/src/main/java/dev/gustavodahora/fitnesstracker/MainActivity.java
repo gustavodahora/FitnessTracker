035793cb6149e06284c2140ebb390c3b9bf567ce
Gustavo da Hora <gustavo.carmodahora@outlook.com>
2021-08-18 10:41:35 -0300

    Adding new string template to the project and add a new imc activity

--- app/src/main/java/dev/gustavodahora/fitnesstracker/MainActivity.java
+++ app/src/main/java/dev/gustavodahora/fitnesstracker/MainActivity.java
@@ -1,13 +1,28 @@
 package dev.gustavodahora.fitnesstracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private View btnImc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnImc = findViewById(R.id.btn_imc);
+
+        btnImc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ImcActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
